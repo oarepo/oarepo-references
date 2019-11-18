66e9ec9e7d73ba47ffae0a91121b3cd4fde1c873
Miroslav Bauer <mirekys@gmail.com>
2019-11-18 17:26:49 +0100

    convert taxonomy to ref in cli command

--- oarepo_references/cli.py
+++ oarepo_references/cli.py
@@ -5,7 +5,9 @@ from invenio_records import Record
 from invenio_records.models import RecordMetadata
 
 from oarepo_references.proxies import current_oarepo_references
-from .signals import update_references_record
+from oarepo_references.utils import transform_dicts_in_data
+from .signals import update_references_record, convert_taxonomy_refs
+
 
 @click.group()
 def references():
@@ -21,6 +23,7 @@ def synchronize():
     """Scan all records and update references table."""
     rms = [v for v, in db.session.query(RecordMetadata.id).all()]
     records = Record.get_records(rms)
-    click.echo('Updating reference records for records: {}'.format(records))
     for rec in records:
+        click.echo('Updating reference records for record: {}'.format(rec.id))
+        transform_dicts_in_data(rec, convert_taxonomy_refs)
         current_oarepo_references.update_references_from_record(rec)
