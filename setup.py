13831562aa188857a2aff4d14ca63266e804b1e9
Miroslav Bauer <mirekys@gmail.com>
2020-08-27 16:56:07 +0200

    fixing damn versions

--- setup.py
+++ setup.py
@@ -36,9 +36,10 @@ setup_requires = [
 ]
 
 install_requires = [
-    'oarepo-validate>=1.1.0'
+    'oarepo-validate>=1.1.0',
     # TODO: remove when tested builds of oarepo are finished
-    'invenio-records-rest==1.6.5'
+    'invenio-records-rest==1.6.5',
+    'werkzeug==0.16.1'
 ]
 
 packages = find_packages()
